84f4c34050c97880506574f81735b4a8c0d66791
Priyanshu <kpriyanshu003@gmail.com>
2021-01-09 00:09:35 -0800

    initial commit

---
+++ user_app/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+#  <li class="nav-item">
+#                     <a class="nav-link" href="{% url 'logout' %}">Logout</a>
+#                 </li>
+
+#                 <li class="nav-item">
+#                     <a class="nav-link" href="{% url 'user_app:user_login' %}">Login</a>
+#                 </li>
